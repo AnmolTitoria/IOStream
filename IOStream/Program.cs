97c49cca3ff3a1352226b20b61f30d51c0f9416c
Anmol Titoriya <anmol.titoriya@acelucid.com>
2022-01-10 22:20:07 +0530

    Setting up demo

---
+++ IOStream/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace IOStream
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+            Write();
+            Read();
+        }
+
+        public static void Write()
+        {
+            StreamWriter SW = new StreamWriter(@"D:\Max 2\Demo.txt");
+            SW.WriteLine("IO Stream Implementation");
+            SW.Close();
+
+        }
+
+        public static void Read()
+        {
+            StreamReader RW = new StreamReader(@"D:\Max 2\Demo.txt");
+            Console.WriteLine(RW.ReadToEnd());
+            RW.Close();
+
+            var files = Directory.GetFiles(@"D:\Max 2\", "*.*", SearchOption.TopDirectoryOnly);
+            foreach (var file in files)
+            {
+               // Console.WriteLine(file);
+               Console.WriteLine(Path.GetFileName(file));
+            }
+
+            string DirPath = @"D:\Max 2\Day2";
+            bool DirSta = Directory.Exists(DirPath);
+
+            if (DirSta)
+            {
+                Console.WriteLine("Yes Exist");
+            }
+            else
+            {
+                Console.WriteLine("Does not exixt");
+                Directory.CreateDirectory(DirPath);
+            }
+
+        }
+    }
+
+
+}
